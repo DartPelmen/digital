e08bba59704f57b241bd099f8f8e3217d6ee054e
DartPelmen <kyzaiv1@yandex.ru>
2019-07-14 15:53:50 +1000

    first

---
+++ app/src/main/java/com/example/myapplication/utils/Sync.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.utils
+
+import androidx.work.Worker
+//Потом дописать - фоновое обновление
+/*
+class Sync : Worker() {
+    override fun doWork(): WorkerResult {
+        getLocal()
+        sync()
+    }
+
+}*/
